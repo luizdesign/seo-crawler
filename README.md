140fbe0ba6396135dd01901e7293a5ea6a9ab8e3
Luiz Kota <luiz.kota@catho.com>
2019-08-05 08:57:41 -0300

    Add command lint interface help and documentation; Its close #1

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ To run the crawler:
 ```sh
 seo-crawler --entrypoint="https://www.dominio.com/"
 ```
+
+### Options
+
+To see the crawler options:
+
+```sh
+seo-crawler --help
+
+Options:
+  -h, --help           output usage information
+  -e, --entrypoint     set the start url to crawler
+```
--- bin/crawler.js
+++ bin/crawler.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
+const program = require('commander');
 const arguments = require('shell-arguments');
-const Crawler = require('../src/index.js');
 
+program.option('-e, --entrypoint', 'set the start url to crawler');
+program.parse(process.argv);
+
+const Crawler = require('../src/index.js');
 const crawler = new Crawler(arguments);
 
 crawler.init();
